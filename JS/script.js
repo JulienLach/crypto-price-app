c841b4557836af09a9adc65701e5fff524aec2e8
JulienLach <leadedchamps@gmail.com>
2024-01-05 16:45:52 +0100

    ok

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,33 @@
 // https://api.coingecko.com/api/v3/simple/price?
 // ids=bitcoin%2Cetherum%2Ccardano%2Clitecoin%2Cdogecoin%2Ctether&vs_currencies=usd&include_24hr_change=true
 
+fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Ctether%2Cethereum%2Clitecoin%2Ccardano%2Cdogecoin&vs_currencies=usd&include_24hr_change=true')
+    .then(reponse => reponse.json())
+    .then(data => {
+        const container = document.querySelector('.container');
+        const coins = Object.getOwnPropertyNames(data); //utilisation de l'objet Object
+        console.log(coins);
+        console.log(data)
 
-fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cetherum%2Ccardano%2Clitecoin%2Cdogecoin%2Ctether&vs_currencies=usd&include_24hr_change=true')
-    .then(response => response.json)
-    .then(json => {
+        for (let coin of coins) { // boucle for pour afficher chaque coin dans le code HTML 1 par 1 avec condition falling ou rising dans la div principale
+            const coinInfo = data[`${coin}`]
+            const price = coinInfo.usd
+            const change = coinInfo.usd_24h_change.toFixed(5);
 
-    });
+            // ajouter de l'HTML directement dans le javascript avec les données récupérées
+            container.innerHTML += `
+            <div class="coin ${change < 0 ? 'falling' : 'rising'}>
+                <div class="coin-logo">
+                        <img src="images/${coin}.png">
+                </div>    
+                <div class="coin"><h3>${coin}</h3>
+                </div>
+                <div class="coin-price">
+                <span class="price">$${price}</span>
+                <span class="change">${change}</span>
+                </div>
+            </div>
+            `
+        }
+
+    })
