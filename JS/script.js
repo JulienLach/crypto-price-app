5532ef13e260028d79e8b3ddbebb77db00642ee0
JulienLach <leadedchamps@gmail.com>
2024-01-05 09:38:05 +0100

    ok

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,9 @@
+// https://api.coingecko.com/api/v3/simple/price?
+// ids=bitcoin%2Cetherum%2Ccardano%2Clitecoin%2Cdogecoin%2Ctether&vs_currencies=usd&include_24hr_change=true
 
+
+fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cetherum%2Ccardano%2Clitecoin%2Cdogecoin%2Ctether&vs_currencies=usd&include_24hr_change=true')
+    .then(response => response.json)
+    .then(json => {
+
+    });
